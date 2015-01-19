4d828b17ce6bdf0bca6836fa8ac43077d98f63cf
Adam Carnagey <adam.carnagey@gravitant.com>
2015-01-18 21:58:35 -0600

    This contains the context files for the introductory course email reply.

--- forms/registration.php
+++ forms/registration.php
@@ -10,13 +10,52 @@ $domain = getenv('MAILGUN_DOMAIN');
 
 $message = "";
 $redirect = "/thank-you.shtml";
+$isNew = false;
+$email = '';
 
 foreach ($_REQUEST as $key => $value) {
   if ($key === "_redirect") {
     $redirect = $value;
     continue;
   }
+	if($key === "email") {
+		$email = $value;
+	}
+	if($key === "first" and $value === "yes") {
+		$isNew = true;
+	}
   $message .= $key . ": " . $value . "\n\r";
+}
+
+if($isNew) {
+	$result = $mg->sendMessage($domain, array(
+			'from'    		=> getenv('ORIGIN_EMAIL'), 
+	    'to'      		=> $email, 
+	    'subject' 		=> getenv('SUBJECT'), 
+	    'text'    		=> file_get_contents('newstudent.txt'),
+			'html'    		=> file_get_contents('newstudent.html'),
+		),
+	  array(
+			'attachment' => array(
+				array(
+					'filePath' => '../doctrine/doctrine_documents/doctrine101.pdf',
+					'remoteName' => 'Doctrine101.pdf'
+				),	
+				array(
+					'filePath' => '../doctrine/doctrine_documents/doctrine101_quiz.pdf',
+					'remoteName' => 'Doctrine101_quiz.pdf'
+				),	
+				array(
+					'filePath' => '../ruth/ruth01.pdf',
+					'remoteName' => 'Ruth01.pdf'
+				),	
+				array(
+					'filePath' => '../titus/titus001.pdf',
+					'remoteName' => 'Titus001.pdf'
+				),				
+			)
+	  )
+	);
 }
 
 $mg->sendMessage($domain, array('from'    => getenv('ORIGIN_EMAIL'), 
