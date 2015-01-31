fbaf3a3dfaa5b15b464134651722b13936f31875
Adam Carnagey <adam@carnagey.com>
2015-01-31 17:12:47 -0600

    Added titus and doctrine to intro email

--- forms/registration.php
+++ forms/registration.php
@@ -61,10 +61,33 @@ if(!empty($errors)) {
   }
 	
   if($isNew) {
+		// Doctine 101
   	$result = $mg->sendMessage($domain, array(
   			'from'    		=> getenv('ORIGIN_EMAIL'),
   	    'to'      		=> $email,
-  	    'subject' 		=> getenv('SUBJECT'),
+  	    'subject' 		=> getenv('SUBJECT').' Doctrine 101',
+  	    'text'    		=> file_get_contents(__DIR__.'/../doctrine/DOC101.email.txt'),
+  			'html'    		=> file_get_contents(__DIR__.'/../doctrine/DOC101.email.html'),
+  		),
+  	  array(
+  			'attachment' => array(
+					array(
+						'filePath' => __DIR__.'/../doctrine/doctrine_documents/doctrine101.pdf',
+						'remoteName' => 'Doctrine101.pdf'
+					),
+					array(
+						'filePath' => __DIR__.'/../doctrine/doctrine_documents/doctrine101_quiz.pdf',
+						'remoteName' => 'Doctrine101_quiz.pdf'
+					),
+  			)
+  	  )
+  	);
+		
+		// Ruth 1
+  	$result = $mg->sendMessage($domain, array(
+  			'from'    		=> getenv('ORIGIN_EMAIL'),
+  	    'to'      		=> $email,
+  	    'subject' 		=> getenv('SUBJECT').' Ruth 1',
   	    'text'    		=> file_get_contents(__DIR__.'/../ruth/RUTH001.email.txt'),
   			'html'    		=> file_get_contents(__DIR__.'/../ruth/RUTH001.email.html'),
   		),
@@ -77,43 +100,32 @@ if(!empty($errors)) {
   			)
   	  )
   	);
+		
+		// Titus 1
+  	$result = $mg->sendMessage($domain, array(
+  			'from'    		=> getenv('ORIGIN_EMAIL'),
+  	    'to'      		=> $email,
+  	    'subject' 		=> getenv('SUBJECT').' Titus 1',
+  	    'text'    		=> file_get_contents(__DIR__.'/../titus/TITUS001.email.txt'),
+  			'html'    		=> file_get_contents(__DIR__.'/../titus/TITUS001.email.html'),
+  		),
+  	  array(
+  			'attachment' => array(
+  				array(
+  					'filePath' => __DIR__.'/../titus/titus001.pdf',
+  					'remoteName' => 'Titus01.pdf'
+  				),
+  			)
+  	  )
+  	);
   }
-
-  // if($isNew) {
-  // 	$result = $mg->sendMessage($domain, array(
-  // 			'from'    		=> getenv('ORIGIN_EMAIL'),
-  // 	    'to'      		=> $email,
-  // 	    'subject' 		=> getenv('SUBJECT'),
-  // 	    'text'    		=> file_get_contents('newstudent.txt'),
-  // 			'html'    		=> file_get_contents('newstudent.html'),
-  // 		),
-  // 	  array(
-  // 			'attachment' => array(
-  // 				array(
-  // 					'filePath' => '../doctrine/doctrine_documents/doctrine101.pdf',
-  // 					'remoteName' => 'Doctrine101.pdf'
-  // 				),
-  // 				array(
-  // 					'filePath' => '../doctrine/doctrine_documents/doctrine101_quiz.pdf',
-  // 					'remoteName' => 'Doctrine101_quiz.pdf'
-  // 				),
-  // 				array(
-  // 					'filePath' => '../ruth/ruth01.pdf',
-  // 					'remoteName' => 'Ruth01.pdf'
-  // 				),
-  // 				array(
-  // 					'filePath' => '../titus/titus001.pdf',
-  // 					'remoteName' => 'Titus001.pdf'
-  // 				),
-  // 			)
-  // 	  )
-  // 	);
-  // }
 
   $mg->sendMessage($domain, array('from'    => getenv('ORIGIN_EMAIL'),
                                   'to'      => getenv('DESTINATION_EMAIL'),
                                   'subject' => getenv('SUBJECT'),
                                   'text'    => $message ));
-  
+																	
+	ob_clean();
   header('Location: ' . $redirect);
+	exit;
 }
--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php ob_start(); ?><!DOCTYPE html>
 
 <html>
 <head>
